c604f8b60a04a11bf5625637480e110112c54145
Akshat Bansal <bansal25@wisc.edu>
2024-09-10 14:22:16 -0700

    Added first attempt at stt

--- app/page.tsx
+++ app/page.tsx
@@ -14,18 +14,40 @@ export default function Home() {
     useEffect(() => {
         const response = getMessage();
     })
+
+    const [transcript, setTranscript] = useState('');
+    const startListening = () => {
+      const SpeechRecognition = (window as any).SpeechRecognition || (window as any).webkitSpeechRecognition;
+      if (!SpeechRecognition) {
+        alert("Browser does not support speech-to-text");
+        return;
+      }
+
+      const recognition = new SpeechRecognition();
+      recognition.lang = 'en-US';
+      recognition.interimResults = true;
+
+      recognition.onresult = (event: any) => {
+        const speechResult = event.results[0][0].transcript;
+        setTranscript(speechResult);
+        console.log('Transcript:', speechResult);
+      };
+
+      recognition.onerror = (event: any) => {
+        console.error('Speech recognition error thrown');
+      };
+
+      recognition.start();
+    };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {message}
       <h1 className="text-white text-4xl mb-4">Welcome! Feel free to ask me questions!</h1>
       <div className="w-auto h-auto">
-        <Image
-          src="/images/iron-man-jarvis-desktop-353i9mfou5bdnzum.jpg"
-          alt="AI Avatar"
-          width={500}
-          height={500}
-          className="rounded-full"
-        />
+        <button onClick={(startListening)} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+          Speech-to-text
+        </button>
+        <p className="mt-4 text-xl">{transcript || 'Waiting for speech...'}</p>
       </div>
       <Link href='/calculator'>
         Calculator
